d46d59fee93f991609ab332ad813453849f637a1
amitpawar <amitpawar7475@gmail.com>
2024-03-29 23:20:15 +0530

    display all of the rooms on the home page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,21 +3,69 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
 
-export function Header() {
+function AccountDropdown() {
   const session = useSession();
+  const isLoggedIn = !!session.data;
 
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => signIn("google")}>Sign In</Button>
-        )}
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ??""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
 
-        {session.data?.user?.name}
-        <ModeToggle />
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" /> Sign Out
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogInIcon className="mr-2" /> Sign In
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="bg-gray-100 dark:bg-gray-900 container mx-auto py-2">
+      <div className="flex justify-between items-center">
+        <div>
+          <Link href="/" className="flex gap-2 items-center text-xl">
+          <Image 
+          src="/icon.png"
+          width="50"
+          height="50"
+          alt="logo-image"
+
+          />
+          Dev Connect
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,21 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
-const queryClient = postgres(process.env.DATABASE_URL!);
-// "postgres://postgres:devconnect@http://localhost:5432/postgres"
-const db = drizzle(queryClient, { schema });
+declare global {
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+let db: PostgresJsDatabase<typeof schema>;
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(postgres(process.env.DATABASE_URL!), { schema });
+} else {
+  if (!global.db) {
+    global.db = drizzle(postgres(process.env.DATABASE_URL!), { schema });
+  }
+
+  db = global.db;
+}
 
 export { db };
